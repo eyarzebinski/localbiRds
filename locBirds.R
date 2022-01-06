757649fe2fad62e4500db5c2b3d49f0807e97695
Evelyn Yarzebinski <eyarzebinski@gmail.com>
2022-01-06 12:20:29 -0500

    init
    code to pull local birds

---
+++ locBirds.R
@@ -0,0 +1,45 @@
+# pull local birds
+
+library(geosphere)
+library(tidyverse)
+library(geosphere)
+library(rebird)
+
+# set date range to grab data from ebird
+dateChoices = seq.Date(as.Date("2015-01-01"), as.Date("2018-12-31"), by = 1)
+
+i = 1
+loc = NULL
+locName = [input nickname eg 'mtRoyal']
+locLat = [input Lat]
+locLng = [input Lng]
+locCounty = [input county eg 'US-PA-003']
+
+# run loop to pull ebird data by specified information above
+for (i in 1:length(dateChoices)) {
+  
+  dayObs = ebirdhistorical(fieldSet = 'full',
+                           loc = locCounty,
+                           date = dateChoices[i],
+                           sleep = 3) # the delay in seconds between pings - reduce server load
+  
+  loc = bind_rows(loc,
+                  dayObs)
+  
+  print(paste0(dateChoices[i]," complete"))
+}
+
+# process the data
+loc_fin = loc %>%
+  filter(subnational2Code == locCounty) %>% # filter for the county - some error cropped up in older years
+  mutate(month = substr(obsDt,6,7),
+         year = substr(obsDt,1,4),
+         id = row_number()-1) %>%
+  group_by(id) %>%
+  mutate(
+    dist_km = geosphere::distm(x = c(locLng,locLat), y = c(lng,lat), fun = distHaversine)) %>%
+  ungroup() %>%
+  mutate(dist_km = as.numeric(dist_km/1000),
+         within5km = ifelse(dist_km<=5, 1, 0))
+
+write.csv(loc_fin, paste0(locName,"_",min(dateChoices),"_",max(dateChoices),".csv"),row.names = F)
